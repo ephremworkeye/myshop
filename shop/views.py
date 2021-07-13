4fc6ff5336e4f793b8ff3a9995da1cc6e55200fb
ephremworkeye <ephremworkeye@gmail.com>
2021-07-13 00:29:43 -0700

    shop view catalog added

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Category, Product
 
 # Create your views here.
+
+def product_list(request, cat_slug=None):
+    category = None
+    categories = Category.objects.all()
+    products = Product.objects.all(available=True)
+    if cat_slug:
+        category = get_object_or_404(Category, slug=cat_slug)
+        products.filter(category=category)
+        return render(request, 'shop/product/list.html', {'category':category, 'categories':categories, 'products':products})
+
+def product_detail(request, id, slug):
+    product = get_object_or_404(Product, id=id, slug=slug, available=True)
+    return render(request, 'shop/product/detail.html', {'product':product})
+    
