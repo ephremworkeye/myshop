6ee73b19fe74e76692576c5fffef3f6220892b5c
ephremworkeye <ephremworkeye@gmail.com>
2021-07-13 01:21:56 -0700

    template structure for shop added and image file served

--- shop/models.py
+++ shop/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 class Category(models.Model):
@@ -10,6 +11,9 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+    
+    def get_absolute_url(self):
+        return reverse('shop:product-list-by-cat', args=[self.slug])
 
 
 class Product(models.Model):
@@ -29,3 +33,6 @@ class Product(models.Model):
     
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse('shop:product-detail', args=[self.id, self.slug])
--- shop/views.py
+++ shop/views.py
@@ -6,11 +6,11 @@ from .models import Category, Product
 def product_list(request, cat_slug=None):
     category = None
     categories = Category.objects.all()
-    products = Product.objects.all(available=True)
+    products = Product.objects.filter(available=True)
     if cat_slug:
         category = get_object_or_404(Category, slug=cat_slug)
         products.filter(category=category)
-        return render(request, 'shop/product/list.html', {'category':category, 'categories':categories, 'products':products})
+    return render(request, 'shop/product/list.html', {'category':category, 'categories':categories, 'products':products})
 
 def product_detail(request, id, slug):
     product = get_object_or_404(Product, id=id, slug=slug, available=True)
