b9a8ba5c51ac4584c8cb626f4a8a3e02dbd3f48f
ephremworkeye <ephremworkeye@gmail.com>
2021-07-13 00:06:46 -0700

    store model for category and product added and registerd to admin

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=150)
+    slug = models.SlugField(max_length=150, unique=True)
+
+    class Meta:
+        ordering = ('name',)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    name = models.CharField(max_length=150)
+    slug = models.SlugField(max_length=150, db_index=True)
+    image = models.ImageField(upload_to='products/%Y/%m/%d',blank=True)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    available = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('name',)
+        index_together = (('id', 'slug'),)
+    
+    def __str__(self):
+        return self.name
