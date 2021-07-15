d8807be3deb38fe1b1896a9a3bd9ab92f958f8a3
ephremworkeye <ephremworkeye@gmail.com>
2021-07-15 16:21:54 -0700

    category, product detail and other static files and template functionality added

--- shop/views.py
+++ shop/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import redirect, render, get_object_or_404
 from .models import Category, Product
 
 # Create your views here.
+
 
 def product_list(request, cat_slug=None):
     category = None
@@ -9,8 +10,8 @@ def product_list(request, cat_slug=None):
     products = Product.objects.filter(available=True)
     if cat_slug:
         category = get_object_or_404(Category, slug=cat_slug)
-        products.filter(category=category)
-    return render(request, 'shop/product/list.html', {'category':category, 'categories':categories, 'products':products})
+        products = products.filter(category=category)
+    return render(request, 'shop/product/list.html', {'category':category, 'categories': categories, 'products':products})
 
 def product_detail(request, id, slug):
     product = get_object_or_404(Product, id=id, slug=slug, available=True)
