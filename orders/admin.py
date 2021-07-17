b58a4cb3bd40e97784fd874f6f08346c158f1dbb
ephremworkeye <ephremworkeye@gmail.com>
2021-07-17 02:23:20 -0700

    models registerd and forms created from model

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Order, OrderItem
 
 # Register your models here.
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    raw_id_fields = ['product']
+
+@admin.register(Order)
+class Order(admin.ModelAdmin):
+    list_display = ('id', 'first_name', 'last_name', 'email', 'address', 'postal_code', 'city', 'is_paid', 'is_delivered', 'updated_at', 'created_at')
+    inlines = [OrderItemInline]
